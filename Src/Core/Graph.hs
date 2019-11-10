9afdfa5c9f5d1405a0c3e0977592e3867c14bdcc
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-10 10:13:23 +0100

    Graph Building

--- Src/Core/Graph.hs
+++ Src/Core/Graph.hs
@@ -2,4 +2,13 @@ module Core.Graph where
 
 import Core.Node
 
-type Graph = Node[]
+type Graph = [Node]
+
+parseSentence:: [String] -> Graph
+parseSentence (w:[]) = (w,Values 1 [] False True) :[]
+parseSentence (w:o:ws) = (w, Values 1 [(o,1)] False False) : parseSentence (o:ws) 
+
+tagStart:: Graph -> [String] -> Graph
+tagStart g (s:ws) = 
+    let t:ts = filter (\(k,v) -> k==s ) g
+    in setStart t : ts
--- Src/Core/Node.hs
+++ Src/Core/Node.hs
@@ -1,5 +1,16 @@
 module Core.Node where 
                             
-type Tag = (Int,Int)
-    
-newtype Node = Int;
+type Node = (String,Values)    
+data Values = Values {
+    magnitude::Int,
+    outs::[(String,Int)],
+    validStart::Bool,
+    validEnd::Bool
+}
+
+(<>) :: Values -> Values -> Values 
+(<>) = undefined
+
+
+setStart :: Node -> Node 
+setStart (k, Values i o _ e) = (k , Values i o True e)
