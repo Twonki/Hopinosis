567cd593d46919bb60942f818eb50349b071b057
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-10 19:34:34 +0100

    Added Parsing to Graph

--- Src/Core/Graph.hs
+++ Src/Core/Graph.hs
@@ -1,8 +1,16 @@
 module Core.Graph where 
 
 import Core.Node
+import Data.List
 
 type Graph = [Node]
+
+parseDocument:: [[String]] -> Graph
+parseDocument = (foldr mergeGraphs []) . map parseSentence'
+
+parseSentence':: [String] -> Graph
+parseSentence' s = let g = parseSentence s
+                   in tagStart g s
 
 parseSentence:: [String] -> Graph
 parseSentence (w:[]) = (w,Values 1 [] False True) :[]
@@ -12,3 +20,23 @@ tagStart:: Graph -> [String] -> Graph
 tagStart g (s:ws) = 
     let t:ts = filter (\(k,v) -> k==s ) g
     in setStart t : ts
+
+mergeGraphs :: Graph -> Graph -> Graph
+mergeGraphs g1 g2 = foldGraph (g1 ++ g2)
+
+foldGraph:: Graph -> Graph 
+foldGraph g = 
+    let 
+        -- Group all Nodes by their key 
+        gs = groupBy ( \ (k,v) (k2,v2) -> k ==k2 ) g
+        -- Make [[(x,v)],...] to [(x,[v]),...]
+        ks = map foldHelp gs
+        -- Apply [v]->v to (x,[v])
+        ks' = map (\(k,vs) -> (k,foldValues vs)) ks
+        -- Return [(x,v)]
+    in ks'
+
+foldHelp :: [(String, Values)] -> (String,[Values])
+foldHelp kvs@((k,_):_) = let key = k 
+                             values =  map (\ (k,v) -> v) kvs 
+                         in (k,values)
--- Src/Core/Node.hs
+++ Src/Core/Node.hs
@@ -6,11 +6,17 @@ data Values = Values {
     outs::[(String,Int)],
     validStart::Bool,
     validEnd::Bool
-}
+    }
+    | EmptyValues
 
-(<>) :: Values -> Values -> Values 
-(<>) = undefined
+valueMerge :: Values -> Values -> Values
+valueMerge EmptyValues b = b
+valueMerge a EmptyValues = a 
+-- Combine the inner values, || for the booleans
+valueMerge (Values i o s e) (Values i2 o2 s2 e2) = Values (i+i2) (o++o2) (s||s2) (e||e2)
 
+foldValues :: [Values] -> Values
+foldValues vs = foldr valueMerge EmptyValues vs 
 
 setStart :: Node -> Node 
 setStart (k, Values i o _ e) = (k , Values i o True e)
