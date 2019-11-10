da1c31d99c36bb4618bb34ba223c44640ba430bf
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-10 19:52:04 +0100

    Removed second constructor for values

--- Src/Core/Node.hs
+++ Src/Core/Node.hs
@@ -7,16 +7,15 @@ data Values = Values {
     validStart::Bool,
     validEnd::Bool
     }
-    | EmptyValues
+
+emptyValues = Values 0 [] False False 
 
 valueMerge :: Values -> Values -> Values
-valueMerge EmptyValues b = b
-valueMerge a EmptyValues = a 
 -- Combine the inner values, || for the booleans
 valueMerge (Values i o s e) (Values i2 o2 s2 e2) = Values (i+i2) (o++o2) (s||s2) (e||e2)
 
 foldValues :: [Values] -> Values
-foldValues vs = foldr valueMerge EmptyValues vs 
+foldValues vs = foldr valueMerge emptyValues vs 
 
 setStart :: Node -> Node 
 setStart (k, Values i o _ e) = (k , Values i o True e)
