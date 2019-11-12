9df41fe44113123e6d2583ab6e51b0a823fa52f8
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-12 22:16:46 +0100

    MergeValues working, but needs love

--- Src/Core/Node.hs
+++ Src/Core/Node.hs
@@ -13,12 +13,20 @@ emptyValues = Values 0 [] False False
 
 mergeValues :: Values -> Values -> Values
 -- Combine the inner values, || for the booleans
-mergeValues (Values i o s e) (Values i2 o2 s2 e2) = Values (i+i2) (o++o2) (s||s2) (e||e2)
+mergeValues (Values i o s e) (Values i2 o2 s2 e2) = Values (i+i2) (mergeOuts o o2) (s||s2) (e||e2)
     where 
         addOut :: [(String,Int)] -> (String,Int) -> [(String,Int)]
-        addOut xs y = undefined
+        addOut xs d@(ky,vy) = if elem ky (map fst xs)
+                                then 
+                                    let hit = filter (\(k,_)-> k== ky) xs 
+                                        nohits = filter (\(k,_)-> k/=ky)  xs
+                                        [t@(_,vx)] = hit
+                                    in (ky,vx+vy) : nohits
+                                else xs++[d]
         mergeOuts :: [(String,Int)] -> [(String,Int)] -> [(String,Int)]
-        mergeOuts xs ys = undefined
+        mergeOuts xs [] = xs
+        mergeOuts xs (y:ys) = let u = (addOut xs y) 
+                              in mergeOuts u ys 
 
 foldValues :: [Values] -> Values
 foldValues vs = foldr mergeValues emptyValues vs 
