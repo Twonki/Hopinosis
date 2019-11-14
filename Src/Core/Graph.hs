75af3a6ece28df36797979e6507b30fc7bae9999
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-14 23:13:08 +0100

    Made Graph with newtype and as a monoid

--- Src/Core/Graph.hs
+++ Src/Core/Graph.hs
@@ -1,51 +1,39 @@
 module Core.Graph where 
 
 import Core.Node
-
 import qualified Data.Map as Map
 
-type Graph = Map.Map String Values
-
-newtype Graph2 = G (Map.Map String Values)
+newtype Graph = G (Map.Map String Values)
     deriving (Eq, Show)
 
-emptyG :: Graph2 
-emptyG = G Map.empty
+instance Semigroup Graph where 
+    (<>) (G a) (G b) = G $ Map.unionWith (<>) a b 
+instance Monoid Graph where
+    mempty = G Map.empty
 
-mappendG :: Graph2 -> Graph2 -> Graph2
-mappendG (G a) (G b) = G $ Map.unionWith (<>) a b 
-
-singleton :: String -> Values -> Graph2
+singleton :: String -> Values -> Graph
 singleton s v= G $ Map.singleton s v
+
+fromNodes :: [Node] -> Graph
+fromNodes nds = mconcat $ map (\(k,v) -> singleton k v) nds
  
-parseSentence2 :: [String] -> Graph2
-parseSentence2 (w:[]) = singleton w (Values 1 Map.empty False True)
-parseSentence2 (w:o:ws) = 
-                let n = singleton w (Values 1 (Map.singleton o 1) False False)
-                in mappendG n (parseSentence2 (o:ws))
-
-tagStart2 :: [String] -> Graph2 -> Graph2
-tagStart2 (s:_) g =  G $ Map.adjust setStart s (unwrap g)
-                
-unwrap:: Graph2 -> Map.Map String  Values
-unwrap (G u) = u 
-
-
 parseDocument:: [[String]] -> Graph
-parseDocument = (foldr merge Map.empty) . map parseSentence'
+parseDocument = mconcat . map parseSentence
 
-parseSentence':: [String] -> Graph
-parseSentence' s = let g = parseSentence s
-                   in tagStart s g
+parseSentence:: [String] -> Graph
+parseSentence s = tagStart s $ parse s
+    where 
+        parse :: [String] -> Graph
+        parse [] = mempty
+        parse (w:[]) = singleton w (Values 1 Map.empty False True)
+        parse (w:o:ws) = 
+                        let n = singleton w (Values 1 (Map.singleton o 1) False False)
+                        in  n <> (parse (o:ws))
+        tagStart :: [String] -> Graph -> Graph
+        tagStart (s:_) =  lift $ Map.adjust setStart s
+        
+lift :: ((Map.Map String Values) -> (Map.Map String Values)) -> Graph -> Graph
+lift f (G u) = G $ f u
 
-parseSentence :: [String] -> Graph
-parseSentence (w:[]) = Map.singleton w (Values 1 Map.empty False True)
-parseSentence (w:o:ws) = 
-                let n = Map.singleton w (Values 1 (Map.singleton o 1) False False)
-                in Map.unionWith (<>) n (parseSentence (o:ws))
-
-tagStart :: [String] -> Graph -> Graph
-tagStart (s:_) = Map.adjust setStart s 
-
-merge :: Graph -> Graph -> Graph
-merge = Map.unionWith (<>)
+unwrap:: Graph -> Map.Map String  Values
+unwrap (G u) = u 
