ef92dc5a164d2c6f9c9e2694957cd017be51228c
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-14 22:58:02 +0100

    WIP Newtyping Graph

--- Src/Core/Graph.hs
+++ Src/Core/Graph.hs
@@ -6,6 +6,31 @@ import qualified Data.Map as Map
 
 type Graph = Map.Map String Values
 
+newtype Graph2 = G (Map.Map String Values)
+    deriving (Eq, Show)
+
+emptyG :: Graph2 
+emptyG = G Map.empty
+
+mappendG :: Graph2 -> Graph2 -> Graph2
+mappendG (G a) (G b) = G $ Map.unionWith (<>) a b 
+
+singleton :: String -> Values -> Graph2
+singleton s v= G $ Map.singleton s v
+ 
+parseSentence2 :: [String] -> Graph2
+parseSentence2 (w:[]) = singleton w (Values 1 Map.empty False True)
+parseSentence2 (w:o:ws) = 
+                let n = singleton w (Values 1 (Map.singleton o 1) False False)
+                in mappendG n (parseSentence2 (o:ws))
+
+tagStart2 :: [String] -> Graph2 -> Graph2
+tagStart2 (s:_) g =  G $ Map.adjust setStart s (unwrap g)
+                
+unwrap:: Graph2 -> Map.Map String  Values
+unwrap (G u) = u 
+
+
 parseDocument:: [[String]] -> Graph
 parseDocument = (foldr merge Map.empty) . map parseSentence'
 
