762d2bf32ebcad652dfc3aad0fd9686648a65a63
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-18 21:47:16 +0100

    Made Graph type instead of newtype Because map of a monoid is already a real and functioning monoid, which is already found by 'type' and doesnt need a newtype

--- Src/Core/Graph.hs
+++ Src/Core/Graph.hs
@@ -4,19 +4,10 @@ import Core.Node
 import qualified Data.Map as Map
 import Data.Text hiding (map,singleton)
 
-newtype Graph = G (Map.Map Text Values)
-    deriving (Eq, Show)
-
-instance Semigroup Graph where 
-    (<>) (G a) (G b) = G $ Map.unionWith (<>) a b 
-instance Monoid Graph where
-    mempty = G Map.empty
-
-singleton :: Text -> Values -> Graph
-singleton s v= G $ Map.singleton s v
+type Graph = Map.Map Text Values
 
 fromNodes :: [Node] -> Graph
-fromNodes nds = mconcat $ map (\(k,v) -> singleton k v) nds
+fromNodes nds = mconcat $ map (\(k,v) -> Map.singleton k v) nds
  
 parseDocument:: [[Text]] -> Graph
 parseDocument = mconcat . map parseSentence
@@ -26,15 +17,9 @@ parseSentence s = tagStart s $ parse s
     where 
         parse :: [Text] -> Graph
         parse [] = mempty
-        parse (w:[]) = singleton w (Values 1 Map.empty False True)
+        parse (w:[]) = Map.singleton w (Values 1 Map.empty False True)
         parse (w:o:ws) = 
-                        let n = singleton w (Values 1 (Map.singleton o 1) False False)
+                        let n = Map.singleton w (Values 1 (Map.singleton o 1) False False)
                         in  n <> (parse (o:ws))
         tagStart :: [Text] -> Graph -> Graph
-        tagStart (s:_) =  lift $ Map.adjust setStart s
-        
-lift :: ((Map.Map Text Values) -> (Map.Map Text Values)) -> Graph -> Graph
-lift f (G u) = G $ f u
-
-unwrap:: Graph -> Map.Map Text  Values
-unwrap (G u) = u 
+        tagStart (s:_) =  Map.adjust setStart s
