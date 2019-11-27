4512bd41aa8aae0f8d0bdf61c9b5a9002b48dbe8
Twonki <Leonhard.Applis@Protonmail.com>
2019-11-27 16:46:44 +0000

    Cosine Similarity and Tests for #14

--- Src/Core/Metric.hs
+++ Src/Core/Metric.hs
@@ -4,8 +4,10 @@ import Core.Path
 import Core.Graph
 import Core.Node   
 
-import qualified Data.Map.Monoidal.Strict as Map
+import qualified Data.Map.Monoidal.Strict as MMap
+import qualified Data.Map.Strict as SMap
 import Data.Text (Text(..))
+import qualified Data.Set as Set
 
 type Metric = Path -> Double
 type GraphAwareMetric = Graph -> Path -> Double
@@ -18,7 +20,7 @@ edgeStrengths p@(x:y:xs)= (fromIntegral $ nextStrength p) + edgeStrengths (y:xs)
         nextStrength :: Path -> Int 
         nextStrength [] = 0
         nextStrength [x] = 0
-        nextStrength (x:y:xs) = (outs $ snd x) Map.! (fst y)
+        nextStrength (x:y:xs) = (outs $ snd x) MMap.! (fst y)
 
 averagedEdgeStrengths :: Metric
 averagedEdgeStrengths p = edgeStrengths p / (fromIntegral $ length p)
@@ -28,3 +30,25 @@ magnitudes p = fromIntegral (sum $ (magnitude .snd) <$> p)
 
 averagedMagnitudes :: Metric 
 averagedMagnitudes p = magnitudes p / (fromIntegral $ length p)
+
+
+toVectors :: (Path,Path) -> ([Double],[Double])
+toVectors (p1,p2) = 
+        (map (\w -> SMap.findWithDefault 0 w p1') unionBagOfWords, map (\w -> SMap.findWithDefault 0 w p2') unionBagOfWords)
+        where 
+            unique = Set.toList . Set.fromList
+            unionBagOfWords = unique $ (fst <$> p1) ++ (fst <$> p2)
+            p1' = SMap.fromAscList $ (\(k,v) -> (k,fromIntegral $ magnitude v)) <$> p1
+            p2' = SMap.fromAscList $ (\(k,v) -> (k,fromIntegral $ magnitude v)) <$> p2
+
+cosineSim :: Path -> Path -> Double
+cosineSim [] _ = 0.0
+cosineSim _ [] = 0.0
+cosineSim p1 p2 = cosine p1vec p2vec
+    where 
+        (p1vec,p2vec) = toVectors (p1,p2)
+        cosine :: [Double] -> [Double] -> Double
+        cosine xs ys = dot xs ys / (len xs * len ys)
+            where 
+                dot a b = sum $ zipWith (*) a b
+                len a = sqrt $ dot a a
--- Test/Tests/MetricTests.hs
+++ Test/Tests/MetricTests.hs
@@ -17,6 +17,12 @@ allMetricTests = TestList [
     ,TestLabel "averagedMagnitudes_DoubleSentenceGraph_shouldBeTwo" averagedMagnitudes_DoubleSentenceGraph_shouldBeTwo
     ,TestLabel "edgeStrength_DoubleSentenceGraph_shouldBeTwoTimesSentenceLengthMinusTwo" edgeStrength_DoubleSentenceGraph_shouldBeTwoTimesSentenceLengthMinusTwo
     ,TestLabel "averagedEdgeStrength_DoubleSentenceGraph_shouldBeBiggerThanOneAndSmallerThanTwo" averagedEdgeStrength_DoubleSentenceGraph_shouldBeBiggerThanOneAndSmallerThanTwo
+
+    ,TestLabel "cosineSim_samePaths_shouldBeOne" cosineSim_samePaths_shouldBeOne
+    ,TestLabel "cosineSim_CompleteDifferentPaths_shouldBeZero" cosineSim_CompleteDifferentPaths_shouldBeZero
+    ,TestLabel "cosineSim_OneEmptyPath_shouldBeZero" cosineSim_OneEmptyPath_shouldBeZero
+    ,TestLabel "cosineSim_TwoEmptyPath_shouldBeZero" cosineSim_TwoEmptyPath_shouldBeZero
+    ,TestLabel "cosineSim_FirstPathEmpty_shouldBeZero" cosineSim_FirstPathEmpty_shouldBeZero
     ]
 
 magnitudes_singleSentenceGraph_shouldBeSentenceLength = 
@@ -75,3 +81,33 @@ averagedEdgeStrength_DoubleSentenceGraph_shouldBeBiggerThanOneAndSmallerThanTwo 
             testGraph = toGraphMany ["Hello I am Test","Hello I am Test"]
             testPaths = allPaths testGraph 
             testPath = head testPaths
+
+cosineSim_samePaths_shouldBeOne = 
+    True ~=? 1>= cosineSim testPath1 testPath2 && cosineSim testPath1 testPath2 >0.99999
+        where 
+            testPath1= packStartNode "Hello" : packNode "to" : packNode "my" :packEndNode "Test" : []
+            testPath2= packStartNode "Hello" : packNode "to" : packNode "my" :packEndNode "Test" : []
+
+cosineSim_CompleteDifferentPaths_shouldBeZero = 
+    0 ~=? cosineSim testPath1 testPath2
+        where 
+            testPath1= packStartNode "Hello" : packNode "to" : packNode "my" :packEndNode "Test" : []
+            testPath2= packStartNode "Bye" : packNode "you" : packNode "fancy" :packEndNode "reader" : []
+
+cosineSim_OneEmptyPath_shouldBeZero = 
+    0 ~=? cosineSim testPath1 testPath2
+        where 
+            testPath1= packStartNode "Hello" : packNode "to" : packNode "my" :packEndNode "Test" : []
+            testPath2= []
+
+cosineSim_TwoEmptyPath_shouldBeZero = 
+    0 ~=? cosineSim testPath1 testPath2
+        where 
+            testPath1= []
+            testPath2= []
+
+cosineSim_FirstPathEmpty_shouldBeZero = 
+    0 ~=? cosineSim testPath1 testPath2
+        where 
+            testPath1= []
+            testPath2= packStartNode "Hello" : packNode "to" : packNode "my" :packEndNode "Test" : []
