207ddc5d8b4601d020725dbb54d2d9c2a3229b27
Twonki <Leonhard.Applis@Protonmail.com>
2020-04-13 19:27:48 +0200

    Added optparsec data object to program, and parser. Added dependency

--- App/Program.hs
+++ App/Program.hs
@@ -11,6 +11,53 @@ import qualified Data.Text as Txt
 import Control.DeepSeq
 import Data.Time 
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+-- The way to parse arguments is taken from 
+-- https://hackage.haskell.org/package/optparse-applicative
+-- Which is a proper library 
+
+data Arguments = Arguments
+  { fpath :: String
+  , verbose :: Bool
+  , n :: Int 
+  , delta :: Double
+  , theta :: Double}
+
+args' :: Parser Arguments
+args' = Arguments
+      <$> strOption
+          ( long "filepath"
+         <> short 'f'
+         <> metavar "fpath"
+         <> help "Filepath to the .txt to summarize" )
+      <*> switch
+          ( long "verbose"
+         <> short 'v'
+         <> help "whether to print debug information" )
+      <*> option auto
+          ( long "summaries"
+         <> short 'n'
+         <> help "number of summary-sentences to be generated"
+         <> showDefault
+         <> value 1
+         <> metavar "INT" )
+    <*> option auto
+          ( long "delta"
+         <> short 'd'
+         <> help "delta threshold for the opinosis algorithm"
+         <> showDefault
+         <> value 0.51
+         <> metavar "DOUBLE" )
+    <*> option auto
+          ( long "theta"
+         <> short 't'
+         <> help "theta threshold for the opinosis algorithm"
+         <> showDefault
+         <> value 0.51
+         <> metavar "DOUBLE" )
+
 main :: IO ()
 main = do 
     [f,n,theta,delta] <- getArgs
